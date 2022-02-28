39b7051cf3644c72e1d1d4b1806e358981183b1f
Nacho <ignacio97orellana@gmail.com>
2022-02-28 19:40:37 -0300

    29 lessons done

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -106,13 +106,13 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-if (num === 10 || 5)
+if (numero === 10 || numero === 5)
 {
-  return "true";
+  return true;
 }
-else
+else 
 {
-  return "false";
+  return false;
 }
 }
 
@@ -120,6 +120,14 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+if (numero < 50 && numero > 20)
+{
+  return true;
+}
+else
+{
+  return false;
+}
 }
 
 function esEntero(numero) {
@@ -130,6 +138,14 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+if (numero % 1 === 0 )
+{
+  return true;
+}
+else
+{
+  return false;
+}
 }
 
 function fizzBuzz(numero) {
@@ -137,6 +153,10 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero % 15 === 0) return 'fizzbuzz';
+  if(numero % 3 === 0) return 'fizz';
+  if(numero % 5 === 0) return 'buzz';
+  return numero;
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -146,6 +166,21 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if(num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+  else {
+    return false;
+  }
 }
 
 function esPrimo(numero) {
@@ -154,6 +189,7 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+
 }
 
 function esVerdadero(valor){
