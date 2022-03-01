0708c2d56ae3d6a18765bf0c879a2195e19b48a8
Nacho <ignacio97orellana@gmail.com>
2022-03-01 19:07:09 -0300

    9 lessons done

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,66 +1,96 @@
 // No cambies los nombres de las funciones.
 
-function devolverPrimerElemento(array) {
+const { convertToRecursiveGlob } = require("@11ty/eleventy/src/TemplatePath");
+
+function devolverPrimerElemento(array)
+{
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array [0];
 }
 
 
-function devolverUltimoElemento(array) {
+function devolverUltimoElemento(array)
+{
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length - 1];
 }
 
-
-function obtenerLargoDelArray(array) {
+function obtenerLargoDelArray(array) 
+{
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
-function incrementarPorUno(array) {
+function incrementarPorUno(array) 
+{
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (var i = 0; i < array.length; i++)
+  {
+    array[i]++;
+  }
+  return array;
 }
 
 
-function agregarItemAlFinalDelArray(array, elemento) {
+function agregarItemAlFinalDelArray(array, elemento)
+{
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 
-function agregarItemAlComienzoDelArray(array, elemento) {
+function agregarItemAlComienzoDelArray(array, elemento) 
+{
   // Añade el "elemento" al comienzo del array
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
 
-function dePalabrasAFrase(palabras) {
+function dePalabrasAFrase(palabras) 
+{
   // "palabras" es un array de strings/cadenas
   // Devuelve un string donde todas las palabras estén concatenadas
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return palabras.join(" ");
 }
 
 
-function arrayContiene(array, elemento) {
+function arrayContiene(array, elemento)
+{
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  return array.includes(elemento);
 }
 
 
-function agregarNumeros(numeros) {
+function agregarNumeros(numeros) 
+{
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var total = 0;
+  for (i = 0; i < numeros.length; i++)
+  {
+    total = total + numeros[i];
+  }
+    return total;
 }
 
 
