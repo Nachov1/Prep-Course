8fda41124c494ca6145ba691ba67bdbf29ec2e9f
Nacho <ignacio97orellana@gmail.com>
2022-03-01 23:09:39 -0300

    21 lessons done :')

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -156,47 +156,95 @@ function cuentoElementos(arreglo)
 }
 
 
-function diaDeLaSemana(numeroDeDia) {
+function diaDeLaSemana(numeroDeDia) 
+{
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia === 1 || numeroDeDia === 7)
+  {
+    return "Es fin de semana";
+  }
+  else
+  {
+    return "Es dia Laboral";
+  }
 } 
 
 
-function empiezaConNueve(n) {
+function empiezaConNueve(n) 
+{
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var num = n + ""
+  if (num.charAt(0) == 9)
+  {
+    return true;
+  }
+    return  false;
 }
 
 
-function todosIguales(arreglo) {
+function todosIguales(arreglo) 
+{
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  var iguales = arreglo [0];
+  for (i = 0; i < arreglo.length; i++)
+    {
+      if(iguales !== arreglo[i])
+        {
+          return false;
+        }
+    }
+  return true;  
 } 
 
 
-function mesesDelAño(array) {
+function mesesDelAño(array) 
+{
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var meses = [];
+  for (i = 0; i < array.length; i++)
+    {
+      if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre")
+        {
+          meses.push(array[i]);
+        } 
+    }
+  if (meses.length < 3)
+    {
+      return "No se encontraron los meses pedidos";
+    } 
+  return meses; 
 }
 
 
-function mayorACien(array) {
+function mayorACien(array) 
+{
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var recorrido = [];
+  for (i = 0; i < array.length; i++)
+    {
+      if (array[i] > 100)
+        {
+          recorrido.push (array[i])   
+        }
+    }
+  return recorrido;  
 }
 
 
-function breakStatement(numero) {
+function breakStatement(numero) 
+{
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array. 
   //Devolver el array
@@ -204,16 +252,48 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var recibido = [];
+  var aumento = numero;
+  for (i = 0; i < 10; i++)
+    {
+      aumento = aumento +2;
+      if (aumento === i)
+        {
+          break;
+        }
+      else
+        {
+          recibido.push(aumento);  
+        }
+    }
+  if (i < 10)
+    {
+      return "Se interrumpió la ejecución"; 
+    }
+  else
+    {
+      return recibido;
+    }
 }
 
 
-function continueStatement(numero) {
+function continueStatement(numero) 
+{
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array.    
   //Devolver el array
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for (i = 0; i < 10; i++)
+    {
+      if (i === 5) continue;
+      suma = suma + 2; 
+      array.push(suma);
+    }
+  return array;  
 }
 
 
